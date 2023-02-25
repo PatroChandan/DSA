cd26a5d4f7faa195779d3118638c5aa206793289
PatroChandan <chandanpatro789@gmail.com>
2023-02-25 13:03:30 +0530

    InfixToPostfix,infixToPrefix

--- src/Stack/InfixToPreFix.java
+++ src/Stack/InfixToPreFix.java
@@ -2,25 +2,26 @@ package Stack;
 
 import java.util.Stack;
 
-/**
- * Question: Infix To postfix
- * Input: "a-b/c - d*c"
- * Output: abc/-dc*-
- */
-public class InfixToPostfix {
+public class InfixToPreFix {
     public static void main(String[] args) {
-        System.out.println(InfixToPostFix("a-b/c-d*c"));
+        System.out.println(Infix2PreFix("a-b/c-d*c"));
     }
-    public static String InfixToPostFix(String str){
+
+    private static String Infix2PreFix(String str) {
+//        String st = "";
+        for (int i = str.length()-1; i < 0; i--) {
+            str = str.charAt(i)+str;
+        }
         StringBuilder sb = new StringBuilder();
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(ch>='a' && ch<='z'){
+            if(ch >='a'&& ch<='z'){
                 sb.append(ch);
             }else {
                 int precedenceOfChar = getPrecedence(ch);
-                while (!stack.isEmpty() && getPrecedence(stack.peek())>=precedenceOfChar){
+                while (!stack.isEmpty() && (getPrecedence(stack.peek())>=precedenceOfChar && ch =='^')
+                        || !stack.isEmpty() && (getPrecedence(stack.peek())>precedenceOfChar)){
                     char popChar = stack.pop();
                     sb.append(popChar);
                 }
@@ -31,19 +32,20 @@ public class InfixToPostfix {
             char popChar = stack.pop();
             sb.append(popChar);
         }
+        for (int i = sb.length()-1; i >=0 ; i--) {
+            sb.append(str.charAt(i));
+        }
         return sb.toString();
     }
 
     private static int getPrecedence(char ch) {
-        if(ch=='-'||ch=='+'){
+        if(ch == '-'||ch=='+'){
             return 1;
-        } else if (ch == '*'||ch == '/') {
+        } else if (ch == '*' || ch == '/') {
             return 2;
         } else if (ch == '^') {
             return 3;
         }
         return 0;
     }
-
-
 }
